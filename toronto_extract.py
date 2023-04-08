21696324992b18d7f7b7977fdcc4aa11680c62df
waleed <waleed.ayoub@gmail.com>
2023-04-08 14:57:56 -0400

    implemented file downloading and saving

--- toronto_extract.py
+++ toronto_extract.py
@@ -2,7 +2,8 @@ import os
 import json
 import requests
 import pandas as pd
-import xlsxwriter
+
+# import xlsxwriter
 
 # Toronto Open Data is stored in a CKAN instance. It's APIs are documented here:
 # https://docs.ckan.org/en/latest/api/
@@ -14,7 +15,10 @@ base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca"
 url = base_url + "/api/3/action/package_show"
 params = {"id": "bike-share-toronto-ridership-data"}
 
+full_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/api/3/action/package_show/?id=bike-share-toronto-ridership-data"
 
+
+# Define a function to get the data from the url
 def get_toronto_data(params: dict) -> json:
     package = requests.get(url, params=params).json()
 
@@ -22,8 +26,29 @@ def get_toronto_data(params: dict) -> json:
         # To get metadata for non datastore_active resources:
         if not resource["datastore_active"]:
             url = base_url + "/api/3/action/resource_show?id=" + resource["id"]
-            resource_metadata = requests.get(url).json()
-    return resource_metadata
+            # resource_metadata contains an entry for each year of data
+            item = requests.get(url).json()
+
+            # Get the URL and file format from the JSON object
+            # Earlier data is in XLSX format and later data is a zip of CSVs separated by month
+            url = item["result"]["url"]
+            file_format = item["result"]["format"]
+            print("URL and FILE FORMAT =========>")
+            print(url, file_format)
+            # Get the year from the filename
+            year = url.split("/")[-1].split("-")[-1].split(".")[0]
+            print(f"we are processing data for year: {year}")
+            # Create a directory for the year if it doesn't exist
+            if not os.path.exists(f"data/toronto/{year}"):
+                os.makedirs(f"data/toronto/{year}")
+            # Download the file
+            filename = f"{year}/{item['result']['name']}"
+            download_file(url, filename)
+            # Convert the file to CSV if it's an Excel file
+            if file_format == "XLSX":
+                excel_to_csv(filename)
+            elif file_format == "ZIP":
+                extract_zip(filename)
 
 
 # Define a function to download the data from the URL and save it to a file
@@ -47,22 +72,5 @@ def excel_to_csv(filename):
 
 
 # Load the JSON data
-with open("data.json", "r") as f:
-    data = json.load(f)
-
-# Loop through each JSON object
-for item in data:
-    # Get the URL and format from the JSON object
-    url = item["result"]["url"]
-    file_format = item["result"]["format"]
-    # Get the year from the filename
-    year = url.split("/")[-1].split("-")[-1].split(".")[0]
-    # Create a directory for the year if it doesn't exist
-    if not os.path.exists(year):
-        os.makedirs(year)
-    # Download the file
-    filename = f"{year}/{item['result']['name']}"
-    download_file(url, filename)
-    # Convert the file to CSV if it's an Excel file
-    if file_format == "XLSX":
-        excel_to_csv(filename)
+# with open("data.json", "r") as f:
+#    data = json.load(f)

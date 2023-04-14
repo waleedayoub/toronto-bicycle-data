91a629210b8cbe481bcfc1a13900fd49c6dd5377
waleed <waleed.ayoub@gmail.com>
2023-04-14 16:27:58 -0400

    finished prefect flows

--- toronto_ridership.py
+++ toronto_ridership.py
@@ -5,6 +5,10 @@ import hashlib
 import zipfile
 import shutil
 
+from pathlib import Path
+
+from datetime import timedelta
+
 import pandas as pd
 
 from prefect import task, flow
@@ -15,12 +19,14 @@ from prefect.tasks import task_input_hash
 
 
 # get a json object from the api url
+@task(name="fetch_api", log_prints=True, retries=3, retry_delay_seconds=30)
 def fetch_api(url: str, params: dict) -> json:
     """Fetch data from an API and return a JSON object"""
     return requests.get(url, params=params).json()
 
 
 # download data to a file from a url
+@task(name="download_file", log_prints=True, retries=3, retry_delay_seconds=30)
 def download_file(url: str, filename: str) -> None:
     """Download a file from a URL and save it to disk"""
     response = requests.get(url, stream=True)
@@ -36,6 +42,7 @@ def download_file(url: str, filename: str) -> None:
 
 
 # Define a function to save each tab in an Excel file as a CSV file
+@task(name="excel_to_csv", log_prints=True, retries=3, retry_delay_seconds=30)
 def excel_to_csv(filename: str, file_location: str) -> None:
     """Go through each worksheet in an Excel workbook and convert each sheet to a CSV file"""
     # Load the Excel file into a Pandas dataframe
@@ -49,6 +56,8 @@ def excel_to_csv(filename: str, file_location: str) -> None:
         df.to_csv(f"{file_location}/{csv_filename}", index=False)
 
 
+# task that extracts zip files (should move this to utils.py)
+@task(name="extract_zip", log_prints=True, retries=3, retry_delay_seconds=30)
 def extract_zip(filename: str, file_location: str) -> None:
     """Extract a zip file to a temporary directory and then move the files to file_location"""
     with zipfile.ZipFile(f"{file_location}/{filename}", "r") as zip_ref:
@@ -64,7 +73,15 @@ def extract_zip(filename: str, file_location: str) -> None:
 
 
 # Define a function to get the data from the url
-def extract_toronto_ridership_data(package: json, file_location: str):
+@task(
+    name="download_toronto_ridership_data",
+    log_prints=True,
+    retries=3,
+    retry_delay_seconds=30,
+    cache_key_fn=task_input_hash,
+    cache_expiration=timedelta(days=1),
+)
+def download_toronto_ridership_data(package: json, file_location: str):
     """Extract the data from the Toronto ridership data package"""
     data_dict = {}
     for _, resource in enumerate(package["result"]["resources"]):
@@ -119,14 +136,59 @@ def extract_toronto_ridership_data(package: json, file_location: str):
 
 
 # create a function to load data to gcs from local
-def load_to_gcs(year: str, file_location: str, filename: str) -> Path:
+@task(name="load_to_gcs", log_prints=True, retries=3, retry_delay_seconds=30)
+def load_to_gcs(file_location: str, year: str, file: str) -> Path:
     """Convert local data to parquet and upload to a GCS bucket, return path to the bucket"""
-    print(f"uploading data to gcs for {file_location}/{year}/{filename}")
+    local_file_path = Path("data/{file_location}/{year}/{file}")
+    df = pd.read_csv(local_file_path)
+    # df.to_parquet(local_file_path)
+    gcs_path = f"data/{file_location}/{year}/{file}.parquet"
+
+    print(f"uploading data to gcs for {file_location}/{year}/{file}")
+    # upload directly from dataframe and skip the write local step:
+    gcs_connection_block = GcsBucket.load("toronto-bikeshare-gcs")
+    gcs_connection_block.upload_from_dataframe(
+        df=df, to_path=gcs_path, serialization_format="parquet"
+    )
+    # if the above doesn't work, uncomment the step where you write the file to parquet locally
+    # gcs_connection_block.upload_from_path(from_path=local_file_path, to_path=gcs_path)
+
+    return Path(f"{gcs_path}")
 
 
 # create a function to load data to bq from gcs
-def load_to_bq():
-    pass
+@task(name="load_to_bq", log_prints=True, retries=3, retry_delay_seconds=30)
+def load_to_bq(file_path: Path):
+    """Load data from GCS to BigQuery"""
+    # load gcp credentials
+    gcp_credentials_block = GcpCredentials.load("de-gcp-creds")
+    result = bigquery_load_cloud_storage(
+        dataset="",
+        table="",
+        uri=file_path,
+        gcp_credentials=gcp_credentials_block.get_credentials_from_service_account(),
+    )
+    return result
+
+
+@flow(name="toronto_ridership_main_flow", log_prints=True)
+def toronto_ridership_main_flow(
+    package_url: str, params: dict, file_location: str
+) -> None:
+    # call fetch_api to get the json object with the correct parameters
+    # this function should be usable for all ckan hosted packages
+    package = fetch_api(package_url, params)
+
+    # download and do some file cleanup for all ridership data, return a dict
+    data_dict = download_toronto_ridership_data(package, file_location)
+    # load data from local to GCS bucket
+    for year, (file_location, filename) in data_dict.items():
+        # only load data for years 2016 or greater
+        if year.isdigit() and int(year) >= 2016:
+            for file in os.listdir(f"{file_location}/{year}"):
+                if file.endswith(".csv") or file.endswith(".zip") and file != filename:
+                    gcs_path = load_to_gcs(file_location, year, file)
+                    load_to_bq(gcs_path)
 
 
 if __name__ == "__main__":
@@ -141,15 +203,4 @@ if __name__ == "__main__":
     params = {"id": "bike-share-toronto-ridership-data"}
     file_location = f"data/toronto/ridership"
 
-    # call fetch_api to get the json object with the correct parameters
-    # this function should be usable for all ckan hosted packages
-    package = fetch_api(package_url, params)
-    # extract data from the json objects and perform some data cleanup
-    data_dict = extract_toronto_ridership_data(package, file_location)
-    # load data from local to GCS bucket
-    for year, (file_location, filenam) in data_dict.items():
-        # only load data for years 2016 or greater
-        if year.isdigit() and int(year) >= 2016:
-            gcs_path = load_to_gcs(year, file_location, filename)
-        # load_to_bq(gcs_path)
-    # load_to_gcs(file_location)
+    toronto_ridership_main_flow(package_url, params, file_location)
